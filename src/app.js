6394138dffff516243c89ffd1da8c514aa7cc2dc
iagolaguna <iagolaguna.83@gmail.com>
2019-01-16 00:25:54 -0200

    sketch and creating components

---
+++ src/app.js
@@ -0,0 +1,35 @@
+'use strict'
+
+import React from 'react'
+import Search from './components/search'
+import UserInfo from './components/user-info'
+import Actions from './components/actions'
+import Repos from './components/repos'
+
+const App = () => (
+  <div className='app'>
+    <Search />
+    <UserInfo />
+    <Actions />
+    <Repos
+      className='repos'
+      title='Repositórios:'
+      repos={[{
+        name: 'Nome do repositorio',
+        link: '#'
+      }]}
+    />
+
+    <Repos
+      className='starred'
+      title='Favoritos:'
+      repos={[{
+        name: 'Nome do repositorio',
+        link: '#'
+      }]}
+    />
+
+  </div>
+)
+
+export default App
